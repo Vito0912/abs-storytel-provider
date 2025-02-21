634aae1f7509d2d4dc7ea9e36dfb1524892f6c51
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2025-02-21 14:45:57 +0100

    changed port to 3010 to not conflict with other providers

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const StorytelProvider = require('./provider');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 const auth = process.env.AUTH;
 
 app.use(cors());
